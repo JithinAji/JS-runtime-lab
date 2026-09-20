1ed6c13107f26f4b7a8f68cd9f140538e04d6062
JithinAji <jithin_aji@outlook.com>
2026-09-20 16:30:20 +0530

    fix: preserve whitespace and apply monospace font to execution output
    Fix to correct this
    function one() {
    console.log("one");
    
        function two() {
            console.log("two");
        }
    
        two();
    
    }
    
    one();
    
    The expected output is
    one
    two
    
    Instead it is
    one two

--- src/script.js
+++ src/script.js
@@ -3,34 +3,34 @@ const outputElement = document.querySelector(".output");
 const codeElement = document.querySelector("textarea");
 
 runButton.addEventListener("click", () => {
-    outputElement.textContent = "";
-    const originalLog = console.log;
+  outputElement.textContent = "";
+  const originalLog = console.log;
 
-    // Intercept console.log calls during evaluation and append output to DOM.
-    console.log = (...args) => {
-        originalLog(...args);
+  // Intercept console.log calls during evaluation and append output to DOM.
+  console.log = (...args) => {
+    originalLog(...args);
 
-        const output = args
-            .map((arg) => {
-                if (typeof arg === "object" && arg !== null) {
-                    return JSON.stringify(arg);
-                }
-                return arg;
-            })
-            .join(" ");
-        outputElement.textContent += `${output}\n`;
-    };
-
-    const codeToRun = codeElement.value;
-    try {
-        const result = eval(codeToRun);
-        if (result !== undefined) {
-            outputElement.textContent += result;
+    const output = args
+      .map((arg) => {
+        if (typeof arg === "object" && arg !== null) {
+          return JSON.stringify(arg);
         }
-    } catch (error) {
-        outputElement.textContent += `${error}\n`;
-    } finally {
-        // Restore original console.log function after evaluation.
-        console.log = originalLog;
+        return arg;
+      })
+      .join(" ");
+    outputElement.textContent += `${output}\n`;
+  };
+
+  const codeToRun = codeElement.value;
+  try {
+    const result = eval(codeToRun);
+    if (result !== undefined) {
+      outputElement.textContent += result;
     }
+  } catch (error) {
+    outputElement.textContent += `${error}\n`;
+  } finally {
+    // Restore original console.log function after evaluation.
+    console.log = originalLog;
+  }
 });
